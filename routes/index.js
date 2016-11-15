897ebebe3ce0345b79499ec126eee5f06d5900df
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-11-14 22:42:20 -0500

    file uploader

--- routes/index.js
+++ routes/index.js
@@ -75,9 +75,7 @@ module.exports = function(router) {
                     if(err) {
                         return res.json({error_code:1,err_desc:err, data: null});
                     } 
-                    res.render('share', {
-                    	json: res.json({error_code:0,err_desc:null, data: result})
-                	});
+                    res.json({error_code:0,err_desc:null, data: result});
                 });
             } catch (e){
                 res.json({error_code:1,err_desc:"Corrupted excel file"});
