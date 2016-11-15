9da408d93587c311380bc117768f7df0ae5ea92e
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-11-15 10:44:59 -0500

    file uploader

--- routes/index.js
+++ routes/index.js
@@ -75,8 +75,9 @@ module.exports = function(router) {
                     if(err) {
                         return res.json({error_code:1,err_desc:err, data: null});
                     } 
+                    var ob = { data:result};
                     res.render('share', {
-                    json: result
+                    json: ob
                     });
                     //res.json({error_code:0,err_desc:null, data: result});
                 });
