5f63a31887f2608cbca3447cbf803f0efaa8915d
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-11-15 21:25:32 -0500

    file uploader format input

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -111,11 +111,28 @@ LabYokeUploader.prototype.upload = function(callback) {
 			var catalognumber = results[prop].catalog_number;
 			var location = results[prop].location;
 			var email = results[prop].user;
-			values = values + "('" + agent
+
+var query = client.query("select * from vm2016_agentsshare where agent='" + agent + "' and vendor='"  + vendor + "' and catalognumber='" + catalognumber + "' and email='"  + email + "'"  );
+		query.on("row", function(row, result) {
+			result.addRow(row);
+		});
+		query.on("end", function(result) {
+			var resultscheck = result.rows;
+			if(resultscheck == null){
+				values = values + "('" + agent + "', '" + vendor + "', '" + catalognumber + "', '" + location + "', '" + email + "','" + now + "')";
+				if(prop < (results.length-1)){
+					values = values + ",";
+				}
+			}
+			console.log("value " + values);
+		});
+
+
+		/*	values = values + "('" + agent
 		+ "', '" + vendor + "', '" + catalognumber + "', '" + location + "', '" + email + "','" + now + "')";
 			if(prop < (results.length-1)){
 				values = values + ",";
-			}
+			}*/
 		}
 	}
 	console.log("values " + values);
