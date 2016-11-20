97c06ab45b721a70077d042f571ac795414d3819
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-11-20 13:34:25 -0500

    pdf implementation

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -121,7 +121,7 @@ LabYokeReporter.prototype.reportSomething = function(callback) {
 		var html = "<div style='float:left; width:50%'><img style='width: 150px; margin: 0 20px;' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke.jpg', alt='The Yoke',  title='Yoke', class='yokelogo'/></div><div style=\"font-family:'calibri'; font-size:11pt;padding: 20px;, width:50%\">"
 				+ "<h1>Shares Uploaded.</h1>";
 		html += "<p>This report is listing all the shares uploaded:</p></div>"
-		html +="<table><tbody><tr style='color: white;background-color: #3d9dcb;'><td>Agent</td><td>Vendor</td><td>Catalog#</td><td>Location</td><td>User</td><td>Category</td></tr>"
+		html +="<table style='font-size: 10pt'><tbody><tr style='color: white;background-color: #3d9dcb;'><td>Agent</td><td>Vendor</td><td>Catalog#</td><td>Location</td><td>User</td><td>Category</td></tr>"
 		if(results != null){
 			for(var prop in results){
 				var agent = results[prop].agent;
