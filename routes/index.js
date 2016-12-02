52c9d1e752c870d9a7033134c21a700591f9e960
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-02 15:36:31 -0500

    add notifications

--- routes/index.js
+++ routes/index.js
@@ -266,7 +266,7 @@ module.exports = function(router) {
 		labYokeAgents.findmyshares(function(error, results) {
 			req.session.orders = results[2];
 			req.session.shares = 0;
-			console.log("is admon? " + req.session.admin);
+			console.log("is orders? " + req.session.orders);
 			res.render('share', {ordersnum: req.session.orders, sharesnum: req.session.shares, labyoker : req.session.user, myshares: results[0], mysharesrequest: results[3], report_sharesbycategory: results[1], loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Share'});
 			req.session.messages = null;
 		});
