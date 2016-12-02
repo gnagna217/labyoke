5a4f2fdcb9be402ead39fbe272e706a2ae0aec3c
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-02 16:25:25 -0500

    add notifications

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -356,7 +356,7 @@ LabYokerGetOrder.prototype.getorders = function(callback) {
 			//results.push(result2.rows);
 
 		var query4 = client
-				.query("update vm2016_orders set status='' where status='new' requestoremail='" + email
+				.query("update vm2016_orders set status='' where status='new' and requestoremail='" + email
 			+ "'");
 		query4.on("row", function(row, result4) {
 			result4.addRow(row);
