6863b098323d239ed8e3003512a5581d5c632120
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-07 20:45:08 -0500

    account implementation

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -362,7 +362,7 @@ LabYokerOrder.prototype.order = function(callback) {
 LabYokerGetOrder.prototype.getLabOrders = function(callback) {
 	var results;
 	console.log("getLabOrders");
-	var query = client.query("SELECT b.lab, count(b.lab) FROM vm2016_orders a, vm2016_users b where a.email = b.email group by b.lab, a.date");
+	var query = client.query("SELECT b.lab, count(b.lab) as counting FROM vm2016_orders a, vm2016_users b where a.email = b.email group by b.lab, a.agent");
 	query.on("row", function(row, result) {
 		result.addRow(row);
 	});
