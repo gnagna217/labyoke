d847474fae33bdcdb54855ce01be3b407e1452d3
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-13 16:39:24 -0500

    testing error page - revert

--- routes/index.js
+++ routes/index.js
@@ -610,7 +610,7 @@ module.exports = function(router) {
 											req.session.orders = orders;
 										}*/
 										console.log("done is " + done);
-										console.log("done2 is " + done.length);
+										//console.log("done2 is " + done.length);
 										console.log("shares is " + shares);
 										console.log("orders is " + orders);
 
