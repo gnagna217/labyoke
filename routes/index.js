d0fbb670a73f87ed83a0f00dd6836a131c28a5ce
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-14 10:58:41 -0500

    implement labs and admins

--- routes/index.js
+++ routes/index.js
@@ -101,7 +101,7 @@ module.exports = function(router) {
 	});
 
 	router.get('/admin', function(req, res) {
-		res.redirect('/share');
+		res.redirect('/share#upload');
 	});
 
 	router.get('/help', /*isLoggedIn,*/ function(req, res) {
