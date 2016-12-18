63a9716dcd5586ec18f767d7c569637b27db67f6
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-17 20:48:28 -0500

    Add report inputs

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -159,7 +159,7 @@ LabYokeReporterSavings.prototype.reportMoney = function(callback) {
 	var selected = "a.category, b.lab";
 	var where = "a.agent = b.agent and a.catalognumber = b.catalognumber ";
 	var groupby = "a.category, b.lab";
-	var columns+="<td>Category</td><td>Lab</td>";
+	var columns ="<td>Category</td><td>Lab</td>";
 	var html = "<div style='text-align:center; width:50%'><img style='width: 150px; margin: 0 20px;' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke4.png', alt='The Yoke',  title='Yoke', class='yokelogo'/></div><div style=\"font-family:'calibri'; font-size:11pt;padding: 20px; width:50%;float:left\">"
 				+ "<h1>Savings.</h1>";
 	console.log("report on savings- datefrom: " + datefrom);
