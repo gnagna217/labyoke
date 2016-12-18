1c6aa9b3e8decea672b7460baa04705a9e49b9b9
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-18 01:47:18 -0500

    fix report

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -360,7 +360,7 @@ LabYokeReporterSavings.prototype.reportMoney = function(callback) {
 				html += " </tr>";
 		
 			}
-			html += "</tbody></table><div>The <span style='font-weight:bold'>Total</span> savings are <span style='font-size:30pt'>$" + savings + ".</span></div><br/><p>&nbsp;</p><p><i><b>The LabYoke Team.</b></i></p><img style='width: 150px; margin: 0 20px;float:left' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke4.png', alt='The Yoke',  title='Yoke', class='yokelogo'/>";
+			html += "</tbody></table><div>The <span style='font-weight:bold'>Total</span> savings are <span style='font-size:30pt'>$" + savings + ".</span></div><br/><p style='margin-top:50px'><i><b>The LabYoke Team.</b></i></p><img style='width: 150px; margin: 0 20px;float:left' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke4.png', alt='The Yoke',  title='Yoke', class='yokelogo'/>";
 			console.log("html money: " + html);
 		}
 		
