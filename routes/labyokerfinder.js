2ca62c8bffe3459d21821cdbe954e6cf431ec85f
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2016-12-18 22:55:22 -0500

    fix issues requested by andria

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -797,7 +797,7 @@ LabYokerGetOrder.prototype.getorders = function(callback) {
 	});
 	query.on("end", function(result) {
 		results.push(result.rows);
-		var query2 = client.query("SELECT b.category, count(b.category) FROM vm2016_orders b where b.lab='"+lab+"' group by b.category");
+		var query2 = client.query("SELECT b.category, count(b.category) FROM vm2016_orders b where b.lab='"+lab+"' and b.insufficient=1 group by b.category");
 		//("SELECT b.category, count(b.category) FROM vm2016_orders a, vm2016_agentsshare b where a.agent = b.agent and a.lab='"+lab+"' group by b.category");
 		query2.on("row", function(row, result2) {
 			result2.addRow(row);
@@ -826,7 +826,7 @@ LabYokerGetOrder.prototype.getorders = function(callback) {
 			results.push(result4.rows);
 			console.log("shares found: " + test3[0].counting)
 
-			var query5 = client.query("SELECT a.category, count(a.category) FROM vm2016_orders a, vm2016_users b where b.lab='"+lab+"' and a.requestoremail=b.email group by a.category");
+			var query5 = client.query("SELECT a.category, count(a.category) FROM vm2016_orders a, vm2016_users b where b.lab='"+lab+"' and a.insufficient=1 and a.requestoremail=b.email group by a.category");
 		query5.on("row", function(row, result5) {
 			result5.addRow(row);
 		});
