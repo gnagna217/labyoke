eab0df583dd4b862bbcc73811f44d88982a42755
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-01-05 14:38:03 -0500

    get lab reports from the list of labs within a department

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ module.exports = function(router) {
                     var labYokeUploader = new LabYokeUploader(result);
                     labYokeUploader.upload(function(error, done) {
                     res.render('share', {
-                    ordersnum: req.session.orders, sharesnum: req.session.shares, json: result, loggedIn : true, isLoggedInAdmin: isLoggedInAdmin, title: 'Share', spreadname: req.file.originalname, labyoker : req.session.user
+                    currentlabname: req.session.lab, ordersnum: req.session.orders, sharesnum: req.session.shares, json: result, loggedIn : true, isLoggedInAdmin: isLoggedInAdmin, title: 'Share', spreadname: req.file.originalname, labyoker : req.session.user
                     });
                 });
                     //res.json({error_code:0,err_desc:null, data: result});
@@ -503,7 +503,7 @@ module.exports = function(router) {
 			//req.session.orders = results[2];
 			req.session.shares = 0;
 			console.log("test ? " + results[3]);
-			res.render('share', {test: results[4], ordersnum: req.session.orders, sharesnum: req.session.shares, labyoker : req.session.user, myshares: results[0], mysharesrequest: results[3], report_sharesbycategory: results[1], loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Share'});
+			res.render('share', {test: results[4], currentlabname: req.session.lab, ordersnum: req.session.orders, sharesnum: req.session.shares, labyoker : req.session.user, myshares: results[0], mysharesrequest: results[3], report_sharesbycategory: results[1], loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Share'});
 			req.session.messages = null;
 		});
 	});
