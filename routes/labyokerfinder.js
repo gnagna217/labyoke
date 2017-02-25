e03155945f5e8cc2b5e32e118179378321d74688
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-02-24 23:03:34 -0500

    adding venn diagram to the shares page initially - get values to populate

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -1017,7 +1017,7 @@ LabYokeAgents.prototype.findmyshares = function(callback) {
 
 						var q = client
 								.query("select b.lab, a.catalognumber, c.labname from vm2016_agentsshare a, vm2016_users b, labs c where c.department='"
-										+ department + "' and a.email = b.email and b.lab = c.labname");
+										+ department + "' and a.email = b.email and b.lab = c.labname order by c.labname");
 	
 
 						console.log("q all reagents in current department: " + q);
