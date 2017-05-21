33127b2586cc149e7f92f4c5cbad227e9e672ba0
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-05-20 22:35:51 -0400

    report intro

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -774,7 +774,7 @@ console.log("report on shares my lab: " + mylab);
 	query.on("end", function(result) {
 		results = result.rows;
 		console.log("results : " + results);
-		var html = "";
+		var html = "<div>";
 		if(results != null && results != ""){
 		html = i18n.__("index.reportsShares.html1", {dateto: dateto}); //"<div><span style='font-weight:bold;font-size:36pt;margin-bottom:20px;float:left'>Inventory.</span></div><div style=\"font-family:'calibri'; font-size:11pt;padding: 20px; width:50%;float:left\">";
 
@@ -811,7 +811,7 @@ console.log("report on shares my lab: " + mylab);
 			//html += "</tbody></table><p><i><b>The LabYoke Team.</b></i></p><img style='width: 141px; margin: 0 20px;float:left' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke4.png', alt='The Yoke',  title='Yoke', class='yokelogo'/>";
 		}
 		if(!isempty){
-			callback(null, html);
+			callback(null, html + "</div>");
 		} else {
 			callback(null, results);
 		}
