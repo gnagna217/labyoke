150903c6df422505fe7d8da0a608ed5c2cc870a2
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-05-21 17:31:01 -0400

    report intro

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -774,9 +774,9 @@ console.log("report on shares my lab: " + mylab);
 	query.on("end", function(result) {
 		results = result.rows;
 		console.log("results : " + results);
-		var html = "";
+		var html = "<div style='margin-top:100px;margin-left:50px;margin-right:20px;margin-bottom:25px;'>";
 		if(results != null && results != ""){
-		html = i18n.__("index.reportsShares.html1", {dateto: dateto}); //"<div><span style='font-weight:bold;font-size:36pt;margin-bottom:20px;float:left'>Inventory.</span></div><div style=\"font-family:'calibri'; font-size:11pt;padding: 20px; width:50%;float:left\">";
+		html += i18n.__("index.reportsShares.html1", {dateto: dateto}); //"<div><span style='font-weight:bold;font-size:36pt;margin-bottom:20px;float:left'>Inventory.</span></div><div style=\"font-family:'calibri'; font-size:11pt;padding: 20px; width:50%;float:left\">";
 
 		if(datefrom == 'all'){
 			html += i18n.__("index.reportsShares.html2"); //"<p>This report is listing all the inventory uploaded:</p></div>"
@@ -811,7 +811,7 @@ console.log("report on shares my lab: " + mylab);
 			//html += "</tbody></table><p><i><b>The LabYoke Team.</b></i></p><img style='width: 141px; margin: 0 20px;float:left' src='https:\/\/team-labyoke.herokuapp.com\/images\/yoke4.png', alt='The Yoke',  title='Yoke', class='yokelogo'/>";
 		}
 		if(!isempty){
-			callback(null, html);
+			callback(null, html + "</div>");
 		} else {
 			callback(null, results);
 		}
