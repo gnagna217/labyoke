37558887d18cca4316318bad98abff43f30ffabc
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-05-26 11:26:55 -0400

    fix email

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -2362,7 +2362,7 @@ LabYokerChangeShare.prototype.cancelShare = function(callback) {
 			body += "</div>";
 			console.log("order body: " + body);
 			var mailOptions = new MailOptionsWithCC(requestor, subject, body, email);
-			//mailOptions.sendAllEmails();
+			mailOptions.sendAllEmails();
 		}
 
 		callback(null, results);
