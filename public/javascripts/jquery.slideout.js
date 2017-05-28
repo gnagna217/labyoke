92553f5152e049a33cb7f9dfe07eafe31eedecbf
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-05-28 17:24:00 -0400

    fix help handle slide

--- public/javascripts/jquery.slideout.js
+++ public/javascripts/jquery.slideout.js
@@ -116,7 +116,7 @@
 
         if(settings.tabLocation === 'right') {
             obj.css({ 'right': '-' + properties.containerWidth});
-            settings.tabHandle.css({'left' : '-' + properties.tabWidth});
+            settings.tabHandle.css({'left' : '-85' /*+ properties.tabWidth*/});
             
             $('html').css('overflow-x', 'hidden');
         }
