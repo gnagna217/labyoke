a78624c99d27457e3572579051add24e5ce04fcf
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-06-02 16:15:27 -0400

    test

---
+++ TestApp/TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by N'Deye Gnagna Marianne Konate on 6/2/17.
+//  Copyright © 2017 N'Deye Gnagna Marianne Konate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
