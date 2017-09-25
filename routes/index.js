500959f71641f6e2947f5b3a2026b45424f76313
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-09-25 15:36:37 -0400

    fix week intro report

--- routes/index.js
+++ routes/index.js
@@ -1122,6 +1122,7 @@ totalshares = t[0].counting;
         var labYokereporter = new LabYokeReporterShares(datefrom, dateto, req.session.lab, req.session.labs,res);
             labYokereporter.reportSharesIntro(function(error, resultsintro) {
                 var res0, res1;
+                res0 = resultsintro[0];
                 console.log("resultsintro: " + resultsintro.length);
                 if(resultsintro !=null && resultsintro.length > 0){
                     res0 = resultsintro[1];
