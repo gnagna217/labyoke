692686c3b0033f1e2194ae4171e5e80174fa1258
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-09-25 15:01:42 -0400

    fix week intro report

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -1025,7 +1025,8 @@ console.log("report on shares my lab: " + mylab);
 			resultsbundled.push(dataonly);
 			callback(null, resultsbundled);
 		} else {
-			callback(null, resultsbundled.push(html + i18n.__("index.reportsShares.nodataIntro") + "<p style='margin-top: 25px;'><i><b>" + i18n.__("index.reportsShares.html7") + "</b></i></p>" + "</div>"));
+			html = html + i18n.__("index.reportsShares.nodataIntro") + "<p style='margin-top: 25px;'><i><b>" + i18n.__("index.reportsShares.html7") + "</b></i></p>" + "</div>";
+			callback(null, resultsbundled.push(html);
 		}
 	});
 });
