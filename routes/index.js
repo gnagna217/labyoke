07d9c0519f2d9ee68ad5f8c6086c0dda9588e844
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-10-02 09:55:02 -0400

    implement sending email notification to owner of insufficient agent - notification setting

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ var xlsxtojson = require("xlsx-to-json-lc");
 
 var storage = multer.diskStorage({ //multers disk storage settings
         destination: function (req, file, cb) {
-            cb(null, './uploads/'
+            cb(null, './uploads/')
         },
         filename: function (req, file, cb) {
             var datetimestamp = Date.now();
