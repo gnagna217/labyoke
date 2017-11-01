ce4ac20000326efaa245b4132f39896f4170de99
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-10-31 23:28:37 -0400

    fix admins list of everyone orders and individual list of orders

--- routes/labyokerfinder.js
+++ routes/labyokerfinder.js
@@ -1679,8 +1679,8 @@ LabYokeAgents.prototype.findallsharesadmins = function(callback) {
 var listlabs = "";
 if(labs){
 for(var l in labs){
-	console.log("l: " + l);
-	listlabs = listlabs + l + ",";
+	console.log("l: " + labs[l]);
+	listlabs = listlabs + labs[l] + ",";
 }
 listlabs = listlabs.replace(/,\s*$/, "")
 console.log("listlabs: " + listlabs);
