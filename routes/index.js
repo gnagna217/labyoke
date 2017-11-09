55f6d29fbcfde3b96cb90462a60f628f448b3c7b
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-11-08 19:10:53 -0500

    bot actions

--- routes/index.js
+++ routes/index.js
@@ -56,6 +56,40 @@ console.log("options: " + options);
     session.send(options, session.message.text);
 });
 
+private Activity HandleSystemMessage(Activity message)
+{
+    if (message.Type == ActivityTypes.DeleteUserData)
+    {
+        // Implement user deletion here
+        // If we handle user deletion, return a real message
+    }
+    else if (message.Type == ActivityTypes.ConversationUpdate)
+    {
+        // Handle conversation state changes, like members being added and removed
+        // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+        // Not available in all channels
+ 
+        // Note: Add introduction here:
+        ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
+        Activity reply = message.CreateReply("Hello from my simple Bot!");
+        connector.Conversations.ReplyToActivityAsync(reply);
+    }
+    else if (message.Type == ActivityTypes.ContactRelationUpdate)
+    {
+        // Handle add/remove from contact lists
+        // Activity.From + Activity.Action represent what happened
+    }
+    else if (message.Type == ActivityTypes.Typing)
+    {
+        // Handle knowing tha the user is typing
+    }
+    else if (message.Type == ActivityTypes.Ping)
+    {
+    }
+ 
+    return null;
+}
+
 
 // Install a custom recognizer to look for user saying 'help' or 'goodbye'.
 bot.recognizer({
