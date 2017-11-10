4d527fbd978b8e15ef308b9e17138f7d490da807
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-11-09 19:17:11 -0500

    bot actions

--- routes/index.js
+++ routes/index.js
@@ -62,10 +62,11 @@ bot.on('conversationUpdate', function (message) {
             if (identity.id == message.address.bot.id) {
                 // Bot is joining conversation
                 // - For WebChat channel you'll get this on page load.
-                var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Welcome to my page");
-                bot.send(reply);
+                var options = session.localizer.gettext(session.preferredLocale(globalocale), "bot.greet");
+                //var reply = new builder.Message()
+                //        .address(message.address)
+                //        .text("Welcome to my page");
+                bot.send(options);
             } else {
                 // User is joining conversation
                 // - For WebChat channel this will be sent when user sends first message.
@@ -74,12 +75,12 @@ bot.on('conversationUpdate', function (message) {
                 //   user we can tweek the address object to reference the joining user.
                 // - If we wanted to send a private message to teh joining user we could
                 //   delete the address.conversation field from the cloned address.
-                var address = Object.create(message.address);
+                /*var address = Object.create(message.address);
                 address.user = identity;
                 var reply = new builder.Message()
                         .address(address)
                         .text("Hello %s", identity.name);
-                bot.send(reply);
+                bot.send(reply);*/
             }
         });
     }
