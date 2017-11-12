a30e412b3630fb1af3099e31c844fa614efde243
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-11-12 13:32:03 -0500

    add swedish

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,11 +1,32 @@
 var shade = document.getElementById("shade");
 var shadelight = document.getElementById("shade-light");
+  var checkedtext = "Do you want to mark this reagent as insufficient?";
+  var uncheckedtext = "Do you want to mark this reagent as replenished and in sufficient quantities?";
+  var checkedtextfull = "Have you fulfilled this order?";
+  var uncheckedtextfull = "Do you want to revert this order fulfillment?";
+  var reagenttext = "Reagent";
+  var vendortext = "Vendor";
+  var catalognumbertext = "Catalog Number";
+  var ownertext = "Owner";
+  var requestortext = "Requestor";
+  var labtext = "Lab";
+  var langText = document.getElementById("langText").innerHTML;
 
- $.getJSON("/javascripts/lang/en.json", function(json) {
-        console.log(json); // access the response object
-        console.log(json.data); // access the array
-        console.log(json.data.popup.order); // access the first object of the array
+
+ $.getJSON("/javascripts/lang/"+langText+".json", function(json) {
+        //console.log(json);
+        console.log(json["popup.order"]);// access the first object of the array
         //console.log(json.data[0].number); // access the first object proprty of the array
+        checkedtext = json["popup.cancel.insufficient"];
+        uncheckedtext = json["popup.cancel.replenish"];
+        checkedtextfull = json["popup.fulfill.order"];
+        uncheckedtextfull = json["popup.fulfill.revert"];
+        reagenttext = json["popup.reagent"];
+        vendortext = json["popup.vendor"];
+        catalognumbertext = json["popup.catalog"];
+        ownertext = json["popup.owner"];
+        requestortext = json["popup.requestor"];
+        labtext = json["popup.lab"];
     });
 
 $('html').click(function() {
@@ -29,8 +50,8 @@ $('.cancel').click(function() {
   console.debug(checked);
   var orderText = document.getElementById("orderText");
   var langText = document.getElementById("langText").innerHTML;
-  var checkedtext = "Do you want to mark this reagent as insufficient?";
-  var uncheckedtext = "Do you want to mark this reagent as replenished and in sufficient quantities?";
+  //var checkedtext = "Do you want to mark this reagent as insufficient?";
+  //var uncheckedtext = "Do you want to mark this reagent as replenished and in sufficient quantities?";
   var desc = "";
   console.log("lang is " + langText );
 
@@ -60,43 +81,43 @@ $('.cancel').click(function() {
   console.log("catalogval: " + catalogval);
   console.log("requestorval: " + requestorval);
 
-  desc += "<br/><b>Reagent:</b> " + agentval;
-   desc += "<br/><b>Vendor:</b> " + vendorval;
-   desc += "<br/><b>Catalog Number:</b> " + catalogval;
-   desc += "<br/><b>Owner:</b> " + owneremailval;
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
       if(selfnode.length > 0){
     selfval = selfnode.val();
   }  
    if(requestornode.length > 0 && selfval == "self"){
     requestorval = requestornode.val();
-    desc += "<br/><b>Requestor:</b> " + requestorval;
+    desc += "<br/><b>"+requestortext+":</b> " + requestorval;
   }  
    checkedtext += desc;
    uncheckedtext += desc;
 
   if(langText == "fr"){
-    checkedtext = "Voulez-vous marquer ce réactif comme insuffisant?";
-   uncheckedtext = "Voulez-vous marquer ce réactif comme réapprovisioné et en quantité suffisante?";
-  desc = "<br/><b>Réactif:</b> " + agentval;
-   desc += "<br/><b>Vendeur:</b> " + vendorval;
-   desc += "<br/><b>Numéro de Catalogue:</b> " + catalogval;
-   desc += "<br/><b>Proprio:</b> " + owneremailval;
+    //checkedtext = "Voulez-vous marquer ce réactif comme insuffisant?";
+   //uncheckedtext = "Voulez-vous marquer ce réactif comme réapprovisioné et en quantité suffisante?";
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
   if(requestorval != ""){
-    desc += "<br/><b>Demandeur:</b> " + requestorval;
+    desc += "<br/><b>"+requestortext+":</b> " + requestorval;
   }
    checkedtext += desc;
    uncheckedtext += desc;
   }
 
   if(langText == "se"){
-    checkedtext = "Önskar du att markera denna agens som otillräcklig?";
-   uncheckedtext = "Önskar du att markera denna agens som fyllt på och i tillräckligt antal?";
-  desc = "<br/><b>Agens:</b> " + agentval;
-   desc += "<br/><b>Försäljare:</b> " + vendorval;
-   desc += "<br/><b>Katalog:</b> " + catalogval;
-   desc += "<br/><b>Ägare:</b> " + owneremailval;
+    //checkedtext = "Önskar du att markera denna agens som otillräcklig?";
+   //uncheckedtext = "Önskar du att markera denna agens som fyllt på och i tillräckligt antal?";
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
   if(requestorval != ""){
-    desc += "<br/><b>Requestor:</b> " + requestorval;
+    desc += "<br/><b>"+requestortext+":</b> " + requestorval;
   }
    checkedtext += desc;
    uncheckedtext += desc;
@@ -143,8 +164,8 @@ $('.fulfill').click(function() {
   var orderText = document.getElementById("orderText");
   var langText = document.getElementById("langText").innerHTML;
   var desc = "";
-  var checkedtext = "Have you fulfilled this order?";
-  var uncheckedtext = "Do you want to revert this order fulfillment?";
+  //var checkedtext = "Have you fulfilled this order?";
+  //var uncheckedtext = "Do you want to revert this order fulfillment?";
   console.log("lang is " + langText );
 
   var agentnode = fulfill.closest('div').next('.agentid');
@@ -168,49 +189,49 @@ $('.fulfill').click(function() {
   console.log("catalogval: " + catalogval);
   console.log("requestorval: " + requestorval);
 
-  desc += "<br/><b>Reagent:</b> " + agentval;
-   desc += "<br/><b>Vendor:</b> " + vendorval;
-   desc += "<br/><b>Catalog Number:</b> " + catalogval;
-   desc += "<br/><b>Owner:</b> " + owneremailval;
-   desc += "<br/><b>Requestor:</b> " + requestorval;
-   desc += "<br/><b>Lab:</b> " + labval;
-   checkedtext += desc;
-   uncheckedtext += desc;
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
+   desc += "<br/><b>"+requestortext+":</b> " + requestorval;
+   desc += "<br/><b>"+labtext+":</b> " + labval;
+   checkedtextfull += desc;
+   uncheckedtextfull += desc;
 
   if(langText == "fr"){
-    checkedtext = "Avez-vous rempli cette commande de réactif?";
-   uncheckedtext = "Voulez-vous marquer cette commande de réactif comme non-remplie?";
-   desc = "<br/><b>Réactif:</b> " + agentval;
-   desc += "<br/><b>Vendeur:</b> " + vendorval;
-   desc += "<br/><b>Numéro de Catalogue:</b> " + catalogval;
-   desc += "<br/><b>Proprio:</b> " + owneremailval;
-   desc += "<br/><b>Demandeur:</b> " + requestorval;
-   desc += "<br/><b>Labo:</b> " + labval;
-   checkedtext += desc;
-   uncheckedtext += desc;
+  //  checkedtextfull = "Avez-vous rempli cette commande de réactif?";
+   //uncheckedtextfull = "Voulez-vous marquer cette commande de réactif comme non-remplie?";
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
+   desc += "<br/><b>"+requestortext+":</b> " + requestorval;
+   desc += "<br/><b>"+labtext+":</b> " + labval;
+   checkedtextfull += desc;
+   uncheckedtextfull += desc;
   }
 
   if(langText == "se"){
-    checkedtext = "Har du fullgjort denna beställning? ";
-   uncheckedtext = "Önskar du att återvända denna beställningsuppfyllelse?";
-   desc = "<br/><b>Agens:</b> " + agentval;
-   desc += "<br/><b>Försäljare:</b> " + vendorval;
-   desc += "<br/><b>Katalog:</b> " + catalogval;
-   desc += "<br/><b>Ägare:</b> " + owneremailval;
-   desc += "<br/><b>Requestor:</b> " + requestorval;
-   desc += "<br/><b>Labo:</b> " + labval;
-   checkedtext += desc;
-   uncheckedtext += desc;
+   // checkedtextfull = "Har du fullgjort denna beställning? ";
+   //uncheckedtextfull = "Önskar du att återvända denna beställningsuppfyllelse?";
+  desc += "<br/><b>"+reagenttext+":</b> " + agentval;
+   desc += "<br/><b>"+vendortext+":</b> " + vendorval;
+   desc += "<br/><b>"+catalognumbertext+":</b> " + catalogval;
+   desc += "<br/><b>"+ownertext+":</b> " + owneremailval;
+   desc += "<br/><b>"+requestortext+":</b> " + requestorval;
+   desc += "<br/><b>"+labtext+":</b> " + labval;
+   checkedtextfull += desc;
+   uncheckedtextfull += desc;
   }
-  console.log("checkedtext is " + checkedtext );
-  console.log("uncheckedtext is " + uncheckedtext );
+  console.log("checkedtext is " + checkedtextfull );
+  console.log("uncheckedtext is " + uncheckedtextfull );
 
 
 
   if(checked){
-    orderText.innerHTML = checkedtext;
+    orderText.innerHTML = checkedtextfull;
   } else {
-    orderText.innerHTML = uncheckedtext;
+    orderText.innerHTML = uncheckedtextfull;
   }
   var pop = document.getElementById("ios-light");
   pop.style.display = "block";
