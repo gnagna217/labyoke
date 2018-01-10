b4c26e0a10a0591c37c68534f282f83a7a25bf5c
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-09 23:51:42 -0500

    cleaning search

--- public/js/search-utils.js
+++ public/js/search-utils.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
+	$(".sleepimg").click(function() {
+	    $( "#bot" ).show();
+	    $( ".awake" ).show();
+	    $( ".sleep" ).hide();
+	    $( "#popup1bot" ).show();
+	    $(".overlaybot").css({ 'visibility' : 'visible', 'opacity' : '1' });
+	    setTimeout(function() { $('.wc-shellinput').focus(); $(".wc-shellinput").attr("placeholder",'#{i18n.__("layout.bot.placeholder")}'); }, 1000);
+	  });
 
-          $(".sleepimg").click(function() {
-                $( "#bot" ).show();
-                $( ".awake" ).show();
-                $( ".sleep" ).hide();
-                $( "#popup1bot" ).show();
-                $(".overlaybot").css({ 'visibility' : 'visible', 'opacity' : '1' });
-                setTimeout(function() { $('.wc-shellinput').focus(); $(".wc-shellinput").attr("placeholder",'#{i18n.__("layout.bot.placeholder")}'); }, 1000);
-              });
-          
-          $(".close").click(function() {
-                $( "#bot" ).hide();
-                $( ".awake" ).hide();
-                $( ".sleep" ).show();
-                $( "#popup1bot" ).hide();
-              });
+	$(".close").click(function() {
+	    $( "#bot" ).hide();
+	    $( ".awake" ).hide();
+	    $( ".sleep" ).show();
+	    $( "#popup1bot" ).hide();
+	  });
 
-          var h = $(window).height() - 125;
-          $(".marginbottom").css("min-height",h);
+	var h = $(window).height() - 125;
+	$(".marginbottom").css("min-height",h);
 
-          });
+});
