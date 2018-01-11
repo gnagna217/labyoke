eda8cc45c354f3274479462f896ba8aec652d5ff
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-11 17:03:21 -0500

    cleaning shares

--- public/js/shares-utils.js
+++ public/js/shares-utils.js
@@ -2,7 +2,169 @@
 
           $(document).ready(function() {
           	
+          $(".sleepimg").click(function() {
+                $( "#bot" ).show();
+                $( ".awake" ).show();
+                $( ".sleep" ).hide();
+                $( "#popup1bot" ).show();
+                $(".overlaybot").css({ 'visibility' : 'visible', 'opacity' : '1' });
+                setTimeout(function() { $('.wc-shellinput').focus(); $(".wc-shellinput").attr("placeholder",'#{i18n.__("layout.bot.placeholder")}'); }, 1000);
+              });
+          
+          $(".close").click(function() {
+                $( "#bot" ).hide();
+                $( ".awake" ).hide();
+                $( ".sleep" ).show();
+                $( "#popup1bot" ).hide();
+              });
 
+          var h = $(window).height() - 125;
+          $(".marginbottom").css("min-height",h);
+
+
+
+
+									var pos = getUrlParameter('pos');
+									console.log("pos: " + pos);
+									if(pos!=null && pos!=""){
+										
+										var tableid = $('#rankTblreqshares').DataTable();
+										$('#rankTblreqshares').removeClass("animated fadeIn");
+										console.debug("jumping");
+										tableid.page.jumpToData(pos, 0);
+										$('#rankTblreqshares').find($( "tr[data-pos="+pos+"]" )).children('td').eq(1).addClass("point");
+
+										$(window).scrollTop($('#rankTblreqshares').offset().top * 10).scrollLeft($('#rankTblreqshares').offset().left);
+									}
+
+
+									var pos2 = getUrlParameter('pos2');
+										console.log("pos2: " + pos2);
+										console.log("pos2_: " + (pos2!=null && pos2!=""));
+										if(pos2!=null && pos2!=""){
+											console.log("jumping0");
+												
+											console.log("jumping1");
+											$('#rankTblshares').removeClass("animated fadeIn");
+											console.log("jumping");
+											var tableid = $('#rankTblshares').DataTable();
+											
+											tableid.page.jumpToData(pos2, 0);
+											$('#rankTblshares').find($("tr[data-pos="+pos2+"]")).children('td').eq(1).addClass("point");
+
+											$(window).scrollTop($('#rankTblshares').offset().top * 10).scrollLeft($('#rankTblshares').offset().left);
+										}
+
+/*				$( ".uploadtext" ).addClass( "animated fadeIn" );
+				$('body').click(function() {
+					// jQuery selector to get an element
+					var query = $('#focusadmin');
+					// check if element is Visible
+					var isVisible = query.is(':visible');
+					if (isVisible === true) {
+						console.log("hide admin focus");
+						query.hide();
+					}
+				});
+			console.log( "share document loaded" );
+				if ($("#checkdisplay").css("display") == "none" ){
+					$('.slide-out-tip-div').hide();
+				} else {
+					$('.slide-out-tip-div').show();
+				}
+				$('#chartNotes').show();
+				$('#sharesVenn').show();
+				$('#sharesAll').show();
+				$('#monthAll').show();
+*/
+
+				$('.slide-out-tip-div').tabSlideOut({
+				imgPos: 100,
+				tabHandle: '.handle',                              //class of the element that will be your tab
+				pathToTabImage: '/images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
+				imageHeight: '50px',                               //height of tab image
+				imageWidth: '50px',                               //width of tab image    
+				tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
+				speed: 300,                                        //speed of animation
+				action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+				topPos: '25% !important',                                   //position from the top
+				fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
+				});
+
+
+
+								});
+
+                        $('#rankTblreqshares').DataTable({order: [[ 5, "desc" ]],
+                          iDisplayLength: 5,
+                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                          language:{
+                          processing: var_processing,
+                          search: var_search,
+                          lengthMenu: var_lengthMenu,
+                          info: var_info,
+                          infoEmpty: var_infoEmpty,
+                          infoFiltered: var_infoFiltered,
+                          infoPostFix: "",
+                          loadingRecords: var_loadingRecords,
+                          zeroRecords: var_zeroRecords,
+                          emptyTable: var_emptyTable,
+                          paginate: {
+                          first: var_first,
+                          previous: var_previous,
+                          next: var_next,
+                          last: var_last,
+                          searchPlaceholder: var_searchplaceholder
+                          },
+                          aria: {
+                          sortAscending: var_sortAscending,
+                          sortDescending: var_sortDescending
+                          }
+                          }
+                        });
+
+
+console.log("rankTblshares: " + $('#rankTblshares').length);
+                        $('#rankTblshares').DataTable({order: [[ 5, "desc" ]],
+                          iDisplayLength: 5,
+                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                          language:{
+                          processing: var_processing,
+                          search: var_search,
+                          lengthMenu: var_lengthMenu,
+                          info: var_info,
+                          infoEmpty: var_infoEmpty,
+                          infoFiltered: var_infoFiltered,
+                          infoPostFix: "",
+                          loadingRecords: var_loadingRecords,
+                          zeroRecords: var_zeroRecords,
+                          emptyTable: var_emptyTable,
+                          paginate: {
+                          first: var_first,
+                          previous: var_previous,
+                          next: var_next,
+                          last: var_last,
+                          searchPlaceholder: var_searchplaceholder
+                          },
+                          aria: {
+                          sortAscending: var_sortAscending,
+                          sortDescending: var_sortDescending
+                          }
+                          }
+                        });
+
+
+
+
+         function getUrlParameter(name) {
+			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+			var results = regex.exec(location.search);
+			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+		};
+
+
+				window.onload = function() {
 				$('#monthAll').hide();
 				$('#sharesAll').hide();
 
@@ -73,168 +235,6 @@
 				}           
 				};
 
+				};
 
 
-                        $('#rankTblreqshares').DataTable({order: [[ 5, "desc" ]],
-                          iDisplayLength: 5,
-                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
-                          language:{
-                          processing: var_processing,
-                          search: var_search,
-                          lengthMenu: var_lengthMenu,
-                          info: var_info,
-                          infoEmpty: var_infoEmpty,
-                          infoFiltered: var_infoFiltered,
-                          infoPostFix: "",
-                          loadingRecords: var_loadingRecords,
-                          zeroRecords: var_zeroRecords,
-                          emptyTable: var_emptyTable,
-                          paginate: {
-                          first: var_first,
-                          previous: var_previous,
-                          next: var_next,
-                          last: var_last,
-                          searchPlaceholder: var_searchplaceholder
-                          },
-                          aria: {
-                          sortAscending: var_sortAscending,
-                          sortDescending: var_sortDescending
-                          }
-                          }
-                        });
-
-
-                        $('#rankTblshares').DataTable({order: [[ 5, "desc" ]],
-                          iDisplayLength: 5,
-                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
-                          language:{
-                          processing: var_processing,
-                          search: var_search,
-                          lengthMenu: var_lengthMenu,
-                          info: var_info,
-                          infoEmpty: var_infoEmpty,
-                          infoFiltered: var_infoFiltered,
-                          infoPostFix: "",
-                          loadingRecords: var_loadingRecords,
-                          zeroRecords: var_zeroRecords,
-                          emptyTable: var_emptyTable,
-                          paginate: {
-                          first: var_first,
-                          previous: var_previous,
-                          next: var_next,
-                          last: var_last,
-                          searchPlaceholder: var_searchplaceholder
-                          },
-                          aria: {
-                          sortAscending: var_sortAscending,
-                          sortDescending: var_sortDescending
-                          }
-                          }
-                        });
-
-
-
-									var pos = getUrlParameter('pos');
-									console.log("pos: " + pos);
-									if(pos!=null && pos!=""){
-										
-										var tableid = $('#rankTblreqshares').DataTable();
-										$('#rankTblreqshares').removeClass("animated fadeIn");
-										console.debug("jumping");
-										tableid.page.jumpToData(pos, 0);
-										$('#rankTblreqshares').find($( "tr[data-pos="+pos+"]" )).children('td').eq(1).addClass("point");
-
-										$(window).scrollTop($('#rankTblreqshares').offset().top * 10).scrollLeft($('#rankTblreqshares').offset().left);
-									}
-
-
-									var pos2 = getUrlParameter('pos2');
-										console.log("pos2: " + pos2);
-										console.log("pos2_: " + (pos2!=null && pos2!=""));
-										if(pos2!=null && pos2!=""){
-											console.log("jumping0");
-												
-											console.log("jumping1");
-											$('#rankTblshares').removeClass("animated fadeIn");
-											console.log("jumping");
-											var tableid = $('#rankTblshares').DataTable();
-											
-											tableid.page.jumpToData(pos2, 0);
-											$('#rankTblshares').find($("tr[data-pos="+pos2+"]")).children('td').eq(1).addClass("point");
-
-											$(window).scrollTop($('#rankTblshares').offset().top * 10).scrollLeft($('#rankTblshares').offset().left);
-										}
-
-				/*$( ".uploadtext" ).addClass( "animated fadeIn" );
-				$('body').click(function() {
-					// jQuery selector to get an element
-					var query = $('#focusadmin');
-					// check if element is Visible
-					var isVisible = query.is(':visible');
-					if (isVisible === true) {
-						console.log("hide admin focus");
-						query.hide();
-					}
-				});
-			console.log( "share document loaded" );
-				if ($("#checkdisplay").css("display") == "none" ){
-					$('.slide-out-tip-div').hide();
-				} else {
-					$('.slide-out-tip-div').show();
-				}
-				$('#chartNotes').show();
-				$('#sharesVenn').show();
-				$('#sharesAll').show();
-				$('#monthAll').show();
-*/
-
-          $(".sleepimg").click(function() {
-                $( "#bot" ).show();
-                $( ".awake" ).show();
-                $( ".sleep" ).hide();
-                $( "#popup1bot" ).show();
-                $(".overlaybot").css({ 'visibility' : 'visible', 'opacity' : '1' });
-                setTimeout(function() { $('.wc-shellinput').focus(); $(".wc-shellinput").attr("placeholder",'#{i18n.__("layout.bot.placeholder")}'); }, 1000);
-              });
-          
-          $(".close").click(function() {
-                $( "#bot" ).hide();
-                $( ".awake" ).hide();
-                $( ".sleep" ).show();
-                $( "#popup1bot" ).hide();
-              });
-
-          var h = $(window).height() - 125;
-          $(".marginbottom").css("min-height",h);
-
-console.log("sliding");
-				$('.slide-out-tip-div').tabSlideOut({
-				imgPos: 100,
-				tabHandle: '.handle',                              //class of the element that will be your tab
-				pathToTabImage: '/images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
-				imageHeight: '50px',                               //height of tab image
-				imageWidth: '50px',                               //width of tab image    
-				tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
-				speed: 300,                                        //speed of animation
-				action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
-				topPos: '25% !important',                                   //position from the top
-				fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
-				});
-								});
-
-
-
-
-
-
-         function getUrlParameter(name) {
-			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-			var results = regex.exec(location.search);
-			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-		};
-
-
-//$(function(){
-
-//			});
