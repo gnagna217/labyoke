1b7786604a6ad6c1a2fc7c359ecd3c42933ef4c4
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-11 18:39:50 -0500

    cleaning search

---
+++ public/js/changepassword-utils.js
@@ -0,0 +1,92 @@
+var h = $(window).height() - 125;
+          $(".marginbottom").css("min-height",h);
+
+
+            $('#changebutton').click(function() {
+                console.log("repass: " + $('#repass').val());
+                console.log("pass: " + $('#pass').val());
+                if($('#pass').val() != "" && $('#repass').val() == $('#pass').val()){
+                    console.log("go on!");
+                    $('#changepwd').submit();
+                } else if($('#pass').val() == ""  || $('#pass').val() == undefined){
+                    console.log("stop!");
+                    $('#messagejs').html("#{i18n.__("index.changepass.message2")}");
+                    $('#messagejs').show();
+                    return false;
+                } else if($('#repass').val() == undefined || $('#repass').val() == undefined){
+                    console.log("stop!");
+                    $('#messagejs').html("#{i18n.__("index.register.message4")}");
+                    $('#messagejs').show();
+                    return false;
+                } 
+            });
+
+            $(document).ready(function(){
+                //$('#tel').mask("(999) 999-9999");
+                $( "#pass" ).focus(function() {
+                  $(this).next(".fa-key").css("color","rgba(61, 157, 203, 0.78)");
+                });
+                $( "#repass" ).focus(function() {
+                  $(this).next(".fa-key").css("color","rgba(61, 157, 203, 0.78)");
+                });
+                var toppinglogin = sessionStorage.getItem('toppinglogin');
+                var topping = sessionStorage.getItem('topping');
+                if(toppinglogin){
+                    $(".login").css("margin-top", toppinglogin + "px");
+                }
+                    
+                $(".login").css("display", "block");
+                var h = $(window).height();
+                var wi = $(window).width();
+
+
+                $(function(){
+                    $.fn.center = function () {
+                    //var topping = $('.topping').attr("title");
+
+                    
+                    var t = ($(window).height() - this.height() ) / 2  ;
+
+                    console.log("topping after: " + topping);
+                    var storedheight = sessionStorage.getItem('storedheight');
+                    
+                    
+                    console.log("h window: " + h);
+                    console.log("sup forgot: " + (topping && parseInt(topping) > 0));
+                    
+                    if(t>0){
+                    sessionStorage.setItem('topping', t - 5);
+                    sessionStorage.setItem('toppinglogin', t - 5);
+                    sessionStorage.setItem('storedheight', h);
+                    }
+                    
+                    console.log("t login: " + t);
+                    console.log("height login : " + this.height());
+                    console.dir(this);
+                    console.log("height login 2 : " + $(".login").height());
+                    if(t > 0){
+                    console.log("topping login: " + topping);
+                    this.css("margin-top",  t + "px");
+                    this.css("-webkit-transition","all .5s ease");
+                    this.css("transition","all .5s ease");
+                    }
+                    
+                    //$('.topping').attr("title",t);
+                    
+                    //this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
+                    return this;
+                    }
+
+                    if(!toppinglogin && wi > 440){
+                        $(".login").center();
+                    }
+
+                    $(window).resize(function(){
+                        if(wi > 440){
+                            sessionStorage.removeItem('toppinglogin');
+                            $(".login").center();
+                        }
+                    });
+
+                });
+            });       
--- public/js/register-utils.js
+++ public/js/register-utils.js
@@ -1,2 +1,37 @@
 var h = $(window).height() - 125;
           $(".marginbottom").css("min-height",h);
+
+$('#messagejs1').hide();
+
+                    $('#regbutton1').click(function() {
+                        console.log("regfirstname: " + $('#regfirstname').val());
+                        console.log("reglastname: " + $('#lastname').val());
+                        console.log("regemail: " + $('#regemail').val());
+                        console.log("regtel: " + $('#tel').val());
+                        if($('#regfirstname').val() != "" && $('#lastname').val() != "" && $('#regemail').val() != "" && $('#tel').val() != "" ){
+                            console.log("go on!");
+                            $('#regform1').submit();
+                        } else if($('#regfirstname').val() == "" || $('#regfirstname').val() == undefined){
+                            console.log("stop!");
+                            $('#messagejs1').html(var_register_7);
+                            $('#messagejs1').show();
+                            return false;
+                        } else if($('#lastname').val() == "" || $('#lastname').val() == undefined){
+                            console.log("stop!");
+                            $('#messagejs1').html(var_register_8);
+                            $('#messagejs1').show();
+                            return false;
+                        } else if($('#regemail').val() == "" || $('#regemail').val() == undefined){
+                            console.log("stop!");
+                            $('#messagejs1').html(var_register_9);
+                            $('#messagejs1').show();
+                            return false;
+                        } else if($('#tel').val() == "" || $('#tel').val() == undefined){
+                            console.log("stop!");
+                            $('#messagejs1').html(var_register_10);
+                            $('#messagejs1').show();
+                            return false;
+                        } 
+                      }); 
+
+                      
