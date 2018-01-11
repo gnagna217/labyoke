67c18b1ab596f0861d9930ea7d38ea0e290cd963
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-10 20:23:31 -0500

    cleaning aboutus

--- public/js/aboutus-utils.js
+++ public/js/aboutus-utils.js
@@ -1,2 +1,33 @@
-var h = $(window).height() - 125;
-          $(".marginbottom").css("min-height",h);
+
+
+        function rotateCard(btn){
+            var $otherbutton;
+            var $card = $(btn).closest('.teamcolumn');
+            var classname = $(btn).parent().attr("class");
+            console.log("classname: " + classname);
+            if(classname == "frontbutton"){
+              $otherbutton = $(btn).parent().next('.backbutton');
+            } else {
+              $otherbutton = $(btn).parent().prev('.frontbutton');
+            }
+            console.log($otherbutton);
+            var $front = $card.find('.front');
+            if($card.hasClass('hover')){
+                $card.removeClass('hover');
+                $front.show();
+                //$(btn).parent().hide();
+                //$otherbutton.show();
+            } else {
+                $card.addClass('hover');
+                $front.hide();
+                //$(btn).parent().hide();
+                //$otherbutton.show();
+            }
+        }
+        $(window).on('load', function() {
+          var h = 200 + $('.fronthelp').height() + $('.section2').height() + $('.front1').height();
+          console.log("container height: " + h);
+          
+          var h0 = $(window).height() - 125;
+          $(".marginbottom").css("min-height",h0);
+        });
