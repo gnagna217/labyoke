7a012f2e8048d83a5ed92106b518bf8fce04a4ef
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-11 13:22:37 -0500

    cleaning orders

--- public/js/orders-utils.js
+++ public/js/orders-utils.js
@@ -24,6 +24,31 @@
 
 
 
+
+                        var win = $( window ).width();
+                        console.log(win);
+                        if(win < 767){
+                          $("#order-container").css( "min-height", "0px" );
+                        }
+                        $("#rankTblOrders").css( "width", "100%" );
+                        
+                      });
+
+         $(function(){
+             $('.slide-out-tip-div').tabSlideOut({
+                 imgPos: 100,
+                 tabHandle: '.handle',                              //class of the element that will be your tab
+                 pathToTabImage: 'images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
+                 imageHeight: '50px',                               //height of tab image
+                 imageWidth: '50px',                               //width of tab image    
+                 tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
+                 speed: 300,                                        //speed of animation
+                 action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+                 topPos: '25% !important',                                   //position from the top
+                 fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
+             });
+         });
+
                         $('#rankTblOrders').DataTable({order: [[ 5, "desc" ]],
                           iDisplayLength: 5,
                           aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
@@ -51,27 +76,3 @@
                           }
                           }
                         });
-                        var win = $( window ).width();
-                        console.log(win);
-                        if(win < 767){
-                          $("#order-container").css( "min-height", "0px" );
-                        }
-                        $("#rankTblOrders").css( "width", "100%" );
-                        
-                      });
-
-         $(function(){
-             $('.slide-out-tip-div').tabSlideOut({
-                 imgPos: 100,
-                 tabHandle: '.handle',                              //class of the element that will be your tab
-                 pathToTabImage: 'images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
-                 imageHeight: '50px',                               //height of tab image
-                 imageWidth: '50px',                               //width of tab image    
-                 tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
-                 speed: 300,                                        //speed of animation
-                 action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
-                 topPos: '25% !important',                                   //position from the top
-                 fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
-             });
-         });
-
