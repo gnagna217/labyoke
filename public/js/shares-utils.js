0284271b3dd28b9eec4e1189ae22b9c5f197eb38
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-11 15:23:30 -0500

    cleaning shares

--- public/js/shares-utils.js
+++ public/js/shares-utils.js
@@ -20,4 +20,216 @@
 
           var h = $(window).height() - 125;
           $(".marginbottom").css("min-height",h);
-          });
+
+
+
+
+									var pos = getUrlParameter('pos');
+									console.log("pos: " + pos);
+									if(pos!=null && pos!=""){
+										
+										var tableid = $('#rankTblreqshares').DataTable();
+										$('#rankTblreqshares').removeClass("animated fadeIn");
+										console.debug("jumping");
+										tableid.page.jumpToData(pos, 0);
+										$('#rankTblreqshares').find($( "tr[data-pos="+pos+"]" )).children('td').eq(1).addClass("point");
+
+										$(window).scrollTop($('#rankTblreqshares').offset().top * 10).scrollLeft($('#rankTblreqshares').offset().left);
+									}
+
+
+									var pos2 = getUrlParameter('pos2');
+										console.log("pos2: " + pos2);
+										console.log("pos2_: " + (pos2!=null && pos2!=""));
+										if(pos2!=null && pos2!=""){
+											console.log("jumping0");
+												
+											console.log("jumping1");
+											$('#rankTblshares').removeClass("animated fadeIn");
+											console.log("jumping");
+											var tableid = $('#rankTblshares').DataTable();
+											
+											tableid.page.jumpToData(pos2, 0);
+											$('#rankTblshares').find($("tr[data-pos="+pos2+"]")).children('td').eq(1).addClass("point");
+
+											$(window).scrollTop($('#rankTblshares').offset().top * 10).scrollLeft($('#rankTblshares').offset().left);
+										}
+
+				$( ".uploadtext" ).addClass( "animated fadeIn" );
+				$('body').click(function() {
+					// jQuery selector to get an element
+					var query = $('#focusadmin');
+					// check if element is Visible
+					var isVisible = query.is(':visible');
+					if (isVisible === true) {
+						console.log("hide admin focus");
+						query.hide();
+					}
+				});
+			console.log( "share document loaded" );
+				if ($("#checkdisplay").css("display") == "none" ){
+					$('.slide-out-tip-div').hide();
+				} else {
+					$('.slide-out-tip-div').show();
+				}
+				$('#chartNotes').show();
+				$('#sharesVenn').show();
+				$('#sharesAll').show();
+				$('#monthAll').show();
+
+
+								});
+
+                        $('#rankTblreqshares').DataTable({order: [[ 5, "desc" ]],
+                          iDisplayLength: 5,
+                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                          language:{
+                          processing: var_processing,
+                          search: var_search,
+                          lengthMenu: var_lengthMenu,
+                          info: var_info,
+                          infoEmpty: var_infoEmpty,
+                          infoFiltered: var_infoFiltered,
+                          infoPostFix: "",
+                          loadingRecords: var_loadingRecords,
+                          zeroRecords: var_zeroRecords,
+                          emptyTable: var_emptyTable,
+                          paginate: {
+                          first: var_first,
+                          previous: var_previous,
+                          next: var_next,
+                          last: var_last,
+                          searchPlaceholder: var_searchplaceholder
+                          },
+                          aria: {
+                          sortAscending: var_sortAscending,
+                          sortDescending: var_sortDescending
+                          }
+                          }
+                        });
+
+
+                        $('#rankTblshares').DataTable({order: [[ 5, "desc" ]],
+                          iDisplayLength: 5,
+                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                          language:{
+                          processing: var_processing,
+                          search: var_search,
+                          lengthMenu: var_lengthMenu,
+                          info: var_info,
+                          infoEmpty: var_infoEmpty,
+                          infoFiltered: var_infoFiltered,
+                          infoPostFix: "",
+                          loadingRecords: var_loadingRecords,
+                          zeroRecords: var_zeroRecords,
+                          emptyTable: var_emptyTable,
+                          paginate: {
+                          first: var_first,
+                          previous: var_previous,
+                          next: var_next,
+                          last: var_last,
+                          searchPlaceholder: var_searchplaceholder
+                          },
+                          aria: {
+                          sortAscending: var_sortAscending,
+                          sortDescending: var_sortDescending
+                          }
+                          }
+                        });
+
+
+
+         function getUrlParameter(name) {
+			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+			var results = regex.exec(location.search);
+			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+		};
+
+
+				window.onload = function() {
+				$('#monthAll').hide();
+				$('#sharesAll').hide();
+
+				$('#chartSelect').val('chartVenn');
+
+				var eSelect = document.getElementById('chartSelect');
+				console.debug("selected: " + $('#chartSelect').val());
+
+				if(chartSelect.selectedIndex === 1 || $('#chartSelect').val() == 'chartVenn') {
+				console.log("sharesVenn inside");
+				$('#chartNotes').hide();
+				$('#sharesVenn').show();
+				$('#monthAll').hide();
+				$('#sharesAll').hide();
+				} if(chartSelect.selectedIndex === 2 || $('#chartSelect').val() == 'chartAll') {
+				console.log("sharesAll inside");
+				$('#chartNotes').hide();
+				$('#sharesAll').show();
+				if("#{find0}" != null && "#{find0}" != undefined && ("#{find0}").length > 0)
+					document.querySelector('.ct-chart').__chartist__.update();
+				$('#monthAll').hide();
+				$('#sharesVenn').hide();
+				} else if(chartSelect.selectedIndex === 3 || $('#reportSelect').val() == 'chartMonth') {
+				console.log("chartOrders inside");
+				$('#chartNotes').hide();
+				$('#sharesAll').hide();
+				$('#monthAll').show();
+				$('#sharesVenn').hide();
+				if("#{filteroutput}" != null && "#{filteroutput}" != undefined && ("#{filteroutput}").length > 0)
+					document.querySelector('.ct-chart0').__chartist__.update();
+				} else if(eSelect.selectedIndex === 0 || $('#reportSelect').val() == 'chartNone') {
+				console.log("result None inside");
+				$('#monthAll').hide();
+				$('#sharesAll').hide();
+				$('#sharesVenn').hide();
+				$('#chartNotes').show();
+				}
+
+				chartSelect.onchange = function() {
+				if(chartSelect.selectedIndex === 1 || $('#chartSelect').val() == 'chartVenn') {
+				console.log("sharesAll inside");
+				$('#chartNotes').hide();
+				$('#sharesAll').hide();
+				$('#sharesVenn').show();
+				$('#monthAll').hide();
+				} if(chartSelect.selectedIndex === 2 || $('#chartSelect').val() == 'chartAll') {
+				console.log("sharesAll inside");
+				$('#chartNotes').hide();
+				$('#sharesAll').show();
+				$('#sharesVenn').hide();
+				if("#{find0}" != null && "#{find0}" != undefined && ("#{find0}").length > 0)
+					document.querySelector('.ct-chart').__chartist__.update();
+				$('#monthAll').hide();
+				}  else if(chartSelect.selectedIndex === 3 || $('#reportSelect').val() == 'chartMonth') {
+				console.log("chartOrders inside");
+				$('#chartNotes').hide();
+				$('#sharesAll').hide();
+				$('#sharesVenn').hide();
+				$('#monthAll').show();
+				if("#{filteroutput}" != null && "#{filteroutput}" != undefined && ("#{filteroutput}").length > 0)
+					document.querySelector('.ct-chart0').__chartist__.update();
+				} else if(eSelect.selectedIndex === 0 || $('#reportSelect').val() == 'chartNone') {
+				console.log("result None inside");
+				$('#monthAll').hide();
+				$('#sharesAll').hide();
+				$('#sharesVenn').hide();
+				$('#chartNotes').show();
+				}           
+				};
+
+				};
+
+
+				$('.slide-out-tip-div').tabSlideOut({
+				imgPos: 100,
+				tabHandle: '.handle',                              //class of the element that will be your tab
+				pathToTabImage: '/images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
+				imageHeight: '50px',                               //height of tab image
+				imageWidth: '50px',                               //width of tab image    
+				tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
+				speed: 300,                                        //speed of animation
+				action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+				topPos: '25% !important',                                   //position from the top
+				fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
+				});
---
+++ public/js/shares-venn-utils.js
@@ -0,0 +1,87 @@
+
+
+							$('#vennChart').jvenn({
+							series: test,
+							colors: colorDefault,
+							searchInput: $("#search-field"),
+							searchStatus: $("#search-status"),
+							searchMinSize: 3,
+							fnClickCallback: function() {
+								$('#pagination-demo').twbsPagination('destroy');
+								var value = "";
+								var title = "";
+								var list = this.list;
+								if (this.listnames.length == 1) {
+									title += "#{i18n.__("shares.venn.chart.title1")}" + "<span class='yokeColor'>";
+								} else {
+									title += "#{i18n.__("shares.venn.chart.title2")}" + "<span class='yokeColor'>";
+								}
+
+								for (name in this.listnames) {
+									title += this.listnames[name] 
+									if(name != (this.listnames.length - 1)){
+										title += " | ";
+									}
+								}
+								title = "<b>" + title + "</span></b>";
+								title += ":";
+								var pageValues = [];
+								if(this.list.length == 0){
+									title +=  "#{i18n.__("shares.venn.chart.titlenone")}" + "";
+								} else {
+									for (val in this.list) {
+										pageValues.push(this.list[val]);
+										value += "<br/>" + this.list[val];
+									}
+								}
+								value = title + value;
+								var total = Math.ceil(this.list.length/3);
+								console.log("total : " + total)
+								var displayvalues = "";
+								for (val in pageValues) {
+									if(val < 3){
+										displayvalues += "<br/>" + pageValues[val];
+										console.log("displayvalues : " + displayvalues)
+									}
+								}
+								if(this.list.length > 1){
+									$('#tooltipsvenn').css('min-height','80px');
+								} else {
+									$('#tooltipsvenn').css('min-height','0px');
+								}
+								if(this.list.length > 3){
+									$('#pagination-demo').twbsPagination({
+										totalPages: total,
+										visiblePages: 3,
+										prev: '<',
+										next: '>',
+										onPageClick: function (event, page) {
+											var displayvalues2 = "";
+											console.log("page is : " + page)
+											console.log("pageValues is : " + pageValues)
+											if(page == 1){
+												for (oval in pageValues) {
+													console.log("oval is : " + oval)
+													if(oval <= ((page*3) - 1)){
+														displayvalues2 += "<br/>" + pageValues[oval];
+														console.log("displayvalues2 is : " + displayvalues2)
+													}
+												}
+												displayvalues2 = title + displayvalues2;
+											} else {
+												for (oval in pageValues) {
+													console.log("oval is : " + oval)
+													if(oval > (((page - 1)*3)-1) && oval <= ((page*3) - 1)){
+														displayvalues2 += "<br/>" + pageValues[oval];
+														console.log("displayvalues2 is : " + displayvalues2)
+													}
+												}
+											}
+											$('#tooltipsvenn').html(displayvalues2);
+										}
+									});
+								}
+
+								$("#tooltipsvenn").html(title + displayvalues);
+							};
+							});
