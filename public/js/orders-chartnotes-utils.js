817019b9742a9f59727642dab31ae7300e4ec251
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-11 11:25:18 -0500

    cleaning orders

---
+++ public/js/orders-chartnotes-utils.js
@@ -0,0 +1,79 @@
+            console.log("find0: " + (find0 != null && find0 != undefined && (find0).length > 0));
+            window.onload = function() {
+            $('#labsComp').hide();
+            $('#monthComp').hide();
+            $('#qtyComp').hide();
+
+            $('#chartSelect').val('chartQty');
+
+            var eSelect = document.getElementById('chartSelect');
+            console.debug("selected: " + $('#chartSelect').val());
+
+            if(chartSelect.selectedIndex === 1 || $('#chartSelect').val() == 'chartQty') {
+              $('#chartNotes').hide();
+              console.log("chartQty inside");
+              $('#qtyComp').show();
+
+              if(find0 != null && find0 != undefined && (find0).length > 0)
+                document.querySelector('.ct-chart1').__chartist__.update();
+              $('#monthComp').hide();
+              $('#labsComp').hide();
+            } else if(chartSelect.selectedIndex === 2 || $('#reportSelect').val() == 'chartOrders') {
+              $('#chartNotes').hide();
+              console.log("chartOrders inside");
+              $('#qtyComp').hide();
+              $('#monthComp').show();
+              if(filteroutput != null && filteroutput != undefined && (filteroutput).length > 0)
+                document.querySelector('.ct-chart0').__chartist__.update();
+              $('#labsComp').hide();
+            } else if(chartSelect.selectedIndex === 3 || $('#reportSelect').val() == 'chartLabs') {
+              $('#chartNotes').hide();
+              console.log("chartLabs inside");
+              $('#qtyComp').hide();
+              $('#monthComp').hide();
+              $('#labsComp').show();
+              if(find2 != null && find2 != undefined && (find2).length > 0)
+                document.querySelector('.ct-chart-3').__chartist__.update();
+            } else if(eSelect.selectedIndex === 0 || $('#reportSelect').val() == 'chartNone') {
+              console.log("result None inside");
+              $('#qtyComp').hide();
+              $('#monthComp').hide();
+              $('#labsComp').hide();
+              $('#chartNotes').show();
+            }
+
+            chartSelect.onchange = function() {
+            if(chartSelect.selectedIndex === 1 || $('#chartSelect').val() == 'chartQty') {
+              console.log("chartQty inside");
+              $('#chartNotes').hide();
+              $('#qtyComp').show();
+              if(find0 != null && find0 != undefined && (find0).length > 0)
+                document.querySelector('.ct-chart1').__chartist__.update();
+              $('#monthComp').hide();
+              $('#labsComp').hide();
+            } else if(chartSelect.selectedIndex === 2 || $('#reportSelect').val() == 'chartOrders') {
+              console.log("chartOrders inside");
+              $('#chartNotes').hide();
+              $('#qtyComp').hide();
+              $('#monthComp').show();
+              if(filteroutput != null && filteroutput != undefined && (filteroutput).length > 0)
+                document.querySelector('.ct-chart0').__chartist__.update();
+              $('#labsComp').hide();
+            } else if(chartSelect.selectedIndex === 3 || $('#reportSelect').val() == 'chartLabs') {
+              console.log("chartLabs inside");
+              $('#chartNotes').hide();
+              $('#qtyComp').hide();
+              $('#monthComp').hide();
+              $('#labsComp').show();
+              if(find2 != null && find2 != undefined && (find2).length > 0)
+                document.querySelector('.ct-chart3').__chartist__.update();
+            } else if(eSelect.selectedIndex === 0 || $('#reportSelect').val() == 'chartNone') {
+              console.log("result None inside");
+              $('#qtyComp').hide();
+              $('#monthComp').hide();
+              $('#labsComp').hide();
+              $('#chartNotes').show();
+            }            
+            };
+
+            };
--- public/js/orders-utils.js
+++ public/js/orders-utils.js
@@ -22,4 +22,56 @@
           $(".marginbottom").css("min-height",h);
 
 
-          });
+
+
+                        $('#rankTblOrders').DataTable({order: [[ 5, "desc" ]],
+                          iDisplayLength: 5,
+                          aLengthMenu: [[5, 10, 15, -1], [5, 10, 15, "All"]],
+                          language:{
+                          processing: var_processing,
+                          search: var_search,
+                          lengthMenu: var_lengthMenu,
+                          info: var_info,
+                          infoEmpty: var_infoEmpty,
+                          infoFiltered: var_infoFiltered,
+                          infoPostFix: "",
+                          loadingRecords: var_loadingRecords,
+                          zeroRecords: var_zeroRecords,
+                          emptyTable: var_emptyTable,
+                          paginate: {
+                          first: var_first,
+                          previous: var_previous,
+                          next: var_next,
+                          last: var_last,
+                          searchPlaceholder: var_searchplaceholder
+                          },
+                          aria: {
+                          sortAscending: var_sortAscending,
+                          sortDescending: var_sortDescending
+                          }
+                          }
+                        });
+                        var win = $( window ).width();
+                        console.log(win);
+                        if(win < 767){
+                          $("#order-container").css( "min-height", "0px" );
+                        }
+                        $("#rankTblOrders").css( "width", "100%" );
+                        
+                      });
+
+         $(function(){
+             $('.slide-out-tip-div').tabSlideOut({
+                 imgPos: 100,
+                 tabHandle: '.handle',                              //class of the element that will be your tab
+                 pathToTabImage: 'images/bouee5.png',          //path to the image for the tab (optionaly can be set using css)
+                 imageHeight: '50px',                               //height of tab image
+                 imageWidth: '50px',                               //width of tab image    
+                 tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
+                 speed: 300,                                        //speed of animation
+                 action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+                 topPos: '25% !important',                                   //position from the top
+                 fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
+             });
+         });
+
