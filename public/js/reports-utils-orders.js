21a3f4cdb86593a877886e05e4a9b77de91adeb6
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-12 21:23:25 -0500

    minified bl

--- public/js/reports-utils-orders.js
+++ public/js/reports-utils-orders.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
                 //var data = [[1,2,3,4],["Sample", "Sample", "Sample", "Sample"],["foo","bar","Hello","0.3"], ["baz", null, "qux"]];
                 console.log("" + var_dataOrders);
 
-                var data = JSON.parse(unescape(var_dataOrders));
+                var data = JSON.parse(escape(var_dataOrders));
                 console.dir(data);
                 var ws_name = "SheetJS";
 
