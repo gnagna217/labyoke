63560cdcfc5063c4d3316b201b99b83d979a231c
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-20 18:11:01 -0500

    database create for any upcoming project

--- db_create/create_admin.sql
+++ db_create/create_admin.sql
@@ -1,35 +1,3 @@
--- Table: public.vm2016_users
-
--- DROP TABLE public.vm2016_users;
-
-CREATE TABLE IF NOT EXISTS public.vm2016_users
-(
-    id text NOT NULL,
-    password text,
-    name text,
-    active integer,
-    changepwd_id text,
-    changepwd_status integer,
-    email text,
-    changepwd_date,
-    lab text,
-    surname,
-    tel text,
-    admin integer,
-    admin_assign text,
-    register_id text,
-    disable integer,
-    lang text,
-    oninsuff integer,
-    onfill integer,
-    CONSTRAINT id_user PRIMARY KEY (id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-
 INSERT INTO public.vm2016_users(
     id, password, name, active, changepwd_id, changepwd_status, email, changepwd_date, lab, surname, tel, admin, admin_assign, register_id, disable, lang, oninsuff, onfill)
-    VALUES ("labyoke", ?, "Lab", 1, ?, ?, "labyoke@gmail.com", ?, ?, "Yoke", ?, 2, ?, ?, ?, "en", ?, ?);
+    VALUES ("labyoke", "$2a$10$cDbJPVVMnqVT44UNc91gYuRScnfkUx.mQNdMmBkP6V.yUFmALdWa6", "Lab", 1, ?, ?, "labyoke@gmail.com", ?, ?, "Yoke", ?, 2, ?, ?, ?, "en", ?, ?);
