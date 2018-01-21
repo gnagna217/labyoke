1f2a112c958df51523e850a6eb5229d2db62d39d
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-21 17:42:15 -0500

    remove electron

--- routes/index.js
+++ routes/index.js
@@ -1161,46 +1161,14 @@ module.exports = function(router) {
 
 
  
-var nc = new notifier.NotificationCenter();
-
-var trueAnswer = 'Most def.';
-
-nc.notify(
-  {
-    title: 'Notifications',
-    message: 'Are they cool?',
-    sound: 'Funk',
-    // case sensitive
-    closeLabel: 'Absolutely not',
-    actions: trueAnswer
-  },
-  function(err, response, metadata) {
-    if (err) throw err;
-    console.log(metadata);
-
-    if (metadata.activationValue !== trueAnswer) {
-      return; // No need to continue
-    }
-
-    nc.notify(
-      {
-        title: 'Notifications',
-        message: 'Do you want to reply to them?',
-        sound: 'Funk',
-        // case sensitive
-        reply: true
-      },
-      function(err, response, metadata) {
-        if (err) throw err;
-        console.log(metadata);
-      }
-    );
-  }
-);
-
-nc.on('replied', function(obj, options, metadata) {
-  console.log('User replied', metadata);
-});
+notifier.notify({ message: 'Hello', wait: true }, function(err, data) {
+    // Will also wait until notification is closed.
+    console.log('Waited');
+    console.log(err, data);
+  })
+  .on('click', function() {
+    console.log(arguments);
+  });
 
 
 
