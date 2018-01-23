11bd2ed5e8d63d1993d6c4ae930022b8bccfb4d5
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-23 10:41:28 -0500

    add notification

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,21 +1,36 @@
-function notify(body, title) {
+function notify(body1, body2, title) {
   // Let's check if the browser supports notifications
-  console.log("sharesnum : " + getCookie("sharesnum"));
-    var options = {
-    body: body,
-    requireInteraction: true
-    //icon: "/images/yoke4.png"
-    };
+  var ns = getCookie("sharesnum");
+  var is = getCookie("is");
+  console.log("sharesnum ns : " + ns);
+  console.log("sharesnum is: " + is);
+
   if (!("Notification" in window)) {
     console.log("This application does not support notification");
   }
 
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
-    if(getCookie("notified") == ""){
+    var ns_ = 0, is_ = 0;
+    var diff = 0;
+    if(ns != null && is !=null){
+        ns_ = parseInt(ns);
+        is_ = parseInt(is);
+        diff = ns_ - is_;
+    }
+    console.log("ns_: " + ns_);
+    console.log("is_: " + is_);
+    console.log("diff: " + diff);
+
+    if(getCookie("notified") == "" || diff > 0){
     // If it's okay let's create a notification
+    var options = {
+    body: body1 + diff + body2,
+    requireInteraction: true
+    //icon: "/images/yoke4.png"
+    };
    var notification = new Notification(title,options); 
-   document.cookie = "notified=true";
+   document.cookie = "notified=true ; is="+ns;
 }
    console.log("here : " + getCookie("notified"));
   }
