bf41f9be870fbe19dfc4ece6e2a40c587a1dd501
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-25 13:16:35 -0500

    fix report

--- public/js/reports-utils.js
+++ public/js/reports-utils.js
@@ -481,7 +481,7 @@ $('.restrictreport').show();
                     source, // HTML string or DOM elem ref.
                     margins.left, // x coord
                     margins.top, { // y coord
-                        'width': 7.5, // max width of content on PDF
+                        'width': margins.width, // max width of content on PDF
                         'elementHandlers': specialElementHandlers
                     },
                     function(dispose) {
