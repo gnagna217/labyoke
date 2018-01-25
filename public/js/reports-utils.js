769f484af64c96a0048342e79ff7d5aa54aa0099
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2018-01-25 16:15:02 -0500

    fix report

--- public/js/reports-utils.js
+++ public/js/reports-utils.js
@@ -476,8 +476,8 @@ $('.restrictreport').show();
                     right: 20,
                     width: 822
                 };
-pdf2.addFont('ComicSansMS', 'Comic Sans', 'normal');
-pdf2.setFont('Comic Sans');
+pdf2.addFont('Nunito', 'Nunito', 'normal');
+pdf2.setFont('Nunito');
 //pdf2.setFont('Nunito');
 //pdf2.setFontType('normal');
                 pdf2.fromHTML(
